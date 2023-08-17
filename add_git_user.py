337cb3f28338cd846676584448d7b0b91cb1af0a
AlexEkdahl <77007088+AlexEkdahl@users.noreply.github.com>
2023-08-17 10:51:18 +0200

    feat: refactor and error handling

--- add_git_user.py
+++ add_git_user.py
@@ -1,60 +1,77 @@
 #!/usr/bin/env python3
+
 import os
 import re
 
-def find_git_config():
-    """Return the path to the .git/config file if it exists, None otherwise."""
-    git_config_path = os.path.join(".git", "config")
-    if os.path.exists(git_config_path):
-        return git_config_path
-    return None
+class GitConfigHandler:
 
-def extract_user_details(content):
-    """Extract user name and email from .git/config content if exists."""
-    user_name_match = re.search(r'name\s*=\s*(.+)', content)
-    user_email_match = re.search(r'email\s*=\s*([\S@.]+)', content)
+    def __init__(self, config_path=".git/config"):
+        self.config_path = config_path
 
-    user_name = user_name_match.group(1).strip() if user_name_match else None
-    user_email = user_email_match.group(1).strip() if user_email_match else None
+    def _get_content(self):
+        try:
+            with open(self.config_path, 'r') as file:
+                return file.read()
+        except FileNotFoundError:
+            raise FileNotFoundError(f"'{self.config_path}' not found.")
+        except Exception as e:
+            raise Exception(f"An error occurred while reading '{self.config_path}': {str(e)}")
 
-    return user_name, user_email
+    def find_git_config(self):
+        """Return the path to the .git/config file if it exists, None otherwise."""
+        return self.config_path if os.path.exists(self.config_path) else None
 
-def override_user_info_in_config(git_config_path, new_name, new_email):
-    """Override the existing user info in the .git/config file."""
-    with open(git_config_path, 'r') as file:
-        content = file.read()
+    def extract_user_details(self):
+        """Extract user name and email from .git/config content if exists."""
+        content = self._get_content()
+        user_name_match = re.search(r'name\s*=\s*(.+)', content)
+        user_email_match = re.search(r'email\s*=\s*([\S@.]+)', content)
 
-    # Replace the existing user details
-    content = re.sub(r'name\s*=\s*.+', f'name = {new_name}', content)
-    content = re.sub(r'email\s*=\s*[\S@.]+', f'email = {new_email}', content)
+        user_name = user_name_match.group(1).strip() if user_name_match else None
+        user_email = user_email_match.group(1).strip() if user_email_match else None
 
-    with open(git_config_path, 'w') as file:
-        file.write(content)
+        return user_name, user_email
+
+    def override_user_info_in_config(self, new_name, new_email):
+        """Override the existing user info in the .git/config file."""
+        content = self._get_content()
+
+        # Replace the existing user details
+        content = re.sub(r'name\s*=\s*.+', f'name = {new_name}', content)
+        content = re.sub(r'email\s*=\s*[\S@.]+', f'email = {new_email}', content)
+
+        try:
+            with open(self.config_path, 'w') as file:
+                file.write(content)
+        except Exception as e:
+            raise Exception(f"An error occurred while writing to '{self.config_path}': {str(e)}")
+
 
 def main():
-    git_config_path = find_git_config()
+    handler = GitConfigHandler()
 
-    if not git_config_path:
-        print("Not a git repository or you're not in the root directory of the repo.")
-        return
-
-    with open(git_config_path, 'r') as file:
-        content = file.read()
-
-    user_name, user_email = extract_user_details(content)
-
-    if user_name and user_email:
-        print(f"Existing user name: {user_name}")
-        print(f"Existing user email: {user_email}")
-        answer = input("Do you want to override the existing user details? (yes/no): ")
-        if answer.lower() not in ["yes", "y"]:
+    try:
+        git_config_path = handler.find_git_config()
+        if not git_config_path:
+            print("Not a git repository or you're not in the root directory of the repo.")
             return
 
-    new_name = "AlexEkdahl"
-    new_email = "77007088+AlexEkdahl@users.noreply.github.com"
+        user_name, user_email = handler.extract_user_details()
+        if user_name and user_email:
+            print(f"Existing user name: {user_name}")
+            print(f"Existing user email: {user_email}")
+            answer = input("Do you want to override the existing user details? (yes/no): ")
+            if answer.lower() not in ["yes", "y"]:
+                return
 
-    override_user_info_in_config(git_config_path, new_name, new_email)
-    print("User details overridden successfully!")
+        new_name = "AlexEkdahl"
+        new_email = "77007088+AlexEkdahl@users.noreply.github.com"
+
+        handler.override_user_info_in_config(new_name, new_email)
+        print("User details overridden successfully!")
+    except Exception as e:
+        print(f"Error: {str(e)}")
+
 
 if __name__ == "__main__":
     main()
