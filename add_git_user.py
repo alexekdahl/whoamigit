ac1641daf736b2bf40aa5f0c8cb9e1366d05497b
AlexEkdahl <77007088+AlexEkdahl@users.noreply.github.com>
2023-08-23 20:32:48 +0200

    updated

--- add_git_user.py
+++ add_git_user.py
@@ -1,78 +1,45 @@
 #!/usr/bin/env python3
 
 import os
-import re
+import inquirer
 
-class GitConfigHandler:
+def get_git_user():
+    name = os.popen('git config user.name').read().strip()
+    email = os.popen('git config user.email').read().strip()
+    return name, email
 
-    def __init__(self, config_path=".git/config"):
-        self.config_path = config_path
-
-    def _get_content(self):
-        try:
-            with open(self.config_path, 'r') as file:
-                return file.read()
-        except FileNotFoundError:
-            raise FileNotFoundError(f"'{self.config_path}' not found.")
-        except Exception as e:
-            raise Exception(f"An error occurred while reading '{self.config_path}': {str(e)}")
-
-    def find_git_config(self):
-        """Return the path to the .git/config file if it exists, None otherwise."""
-        return self.config_path if os.path.exists(self.config_path) else None
-
-    def extract_user_details(self):
-        """Extract user name and email from .git/config content if exists."""
-        content = self._get_content()
-        user_name_match = re.search(r'name\s*=\s*(.+)', content)
-        user_email_match = re.search(r'email\s*=\s*([\S@.]+)', content)
-
-        user_name = user_name_match.group(1).strip() if user_name_match else None
-        user_email = user_email_match.group(1).strip() if user_email_match else None
-
-        return user_name, user_email
-
-    def override_user_info_in_config(self, new_name, new_email):
-        """Override the existing user info or insert new info in the .git/config file."""
-        content = self._get_content()
-
-        # Check if user details exist
-        if re.search(r'name\s*=\s*.+', content) and re.search(r'email\s*=\s*[\S@.]+', content):
-            content = re.sub(r'name\s*=\s*.+', f'name = {new_name}', content)
-            content = re.sub(r'email\s*=\s*[\S@.]+', f'email = {new_email}', content)
-        else:  # If not, append user details
-            content += f"\n[user]\n\tname = {new_name}\n\temail = {new_email}\n"
-
-        try:
-            with open(self.config_path, 'w') as file:
-                file.write(content)
-        except Exception as e:
-            raise Exception(f"An error occurred while writing to '{self.config_path}': {str(e)}")
+def set_git_user(name, email):
+    os.system(f'git config user.name "{name}"')
+    os.system(f'git config user.email "{email}"')
 
 def main():
-    handler = GitConfigHandler()
+    current_name, current_email = get_git_user()
 
-    try:
-        git_config_path = handler.find_git_config()
-        if not git_config_path:
-            print("Not a git repository or you're not in the root directory of the repo.")
-            return
+    if current_name and current_email:
+        print(f"Current git user for this project:\nName: {current_name}\nEmail: {current_email}\n")
+    else:
+        print("No local git user set for this project.\n")
 
-        user_name, user_email = handler.extract_user_details()
-        if user_name and user_email:
-            print(f"Existing user name: {user_name}")
-            print(f"Existing user email: {user_email}")
-            answer = input("Do you want to override the existing user details? (yes/no): ")
-            if answer.lower() not in ["yes", "y"]:
-                return
+    questions = [
+        inquirer.List('user',
+                      message="Which git user do you want to use for this project?",
+                      choices=['Personal', 'Work'],
+                      ),
+    ]
 
-        new_name = "AlexEkdahl"
-        new_email = "77007088+AlexEkdahl@users.noreply.github.com"
+    answers = inquirer.prompt(questions)
 
-        handler.override_user_info_in_config(new_name, new_email)
-        print("User details overridden successfully!")
-    except Exception as e:
-        print(f"Error: {str(e)}")
+    # Check if answers is None (user canceled the prompt)
+    if answers is None:
+        print("Operation canceled by user.")
+        return
 
-if __name__ == "__main__":
+    if answers['user'] == 'Personal':
+        set_git_user('AlexEkdahl', '77007088+AlexEkdahl@users.noreply.github.com')
+        print('Set to Personal git user for this project.')
+    elif answers['user'] == 'Work':
+        set_git_user('Work Name', 'work@email.com')
+        print('Set to Work git user for this project.')
+
+if __name__ == '__main__':
     main()
