839ec15c8ebfc9376cd7c6824c03be35119c2acd
Alexander Ekdahl <alexander.ekdahl@assaabloy.com>
2023-11-02 12:34:08 +0100

    venv

--- add_git_user.py
+++ add_git_user.py
@@ -3,14 +3,17 @@
 import os
 import inquirer
 
+
 def get_git_user():
-    name = os.popen('git config user.name').read().strip()
-    email = os.popen('git config user.email').read().strip()
+    name = os.popen("git config user.name").read().strip()
+    email = os.popen("git config user.email").read().strip()
     return name, email
+
 
 def set_git_user(name, email):
     os.system(f'git config user.name "{name}"')
     os.system(f'git config user.email "{email}"')
+
 
 def main():
     current_name, current_email = get_git_user()
@@ -21,10 +24,11 @@ def main():
         print("No local git user set for this project.\n")
 
     questions = [
-        inquirer.List('user',
-                      message="Which git user do you want to use for this project?",
-                      choices=['Personal', 'Work'],
-                      ),
+        inquirer.List(
+            "user",
+            message="Which git user do you want to use for this project?",
+            choices=["Personal", "Work"],
+        ),
     ]
 
     answers = inquirer.prompt(questions)
@@ -33,12 +37,13 @@ def main():
     if answers is None:
         return
 
-    if answers['user'] == 'Personal':
-        set_git_user('AlexEkdahl', '77007088+AlexEkdahl@users.noreply.github.com')
-        print('Set to Personal git user for this project.')
-    elif answers['user'] == 'Work':
-        set_git_user('Work Name', 'work@email.com')
-        print('Set to Work git user for this project.')
+    if answers["user"] == "Personal":
+        set_git_user("alexekdahl", "77007088+alexekdahl@users.noreply.github.com")
+        print("Set to Personal git user for this project.")
+    elif answers["user"] == "Work":
+        set_git_user("Work Name", "work@email.com")
+        print("Set to Work git user for this project.")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
